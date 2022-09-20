73e2ff23f67b2e4ff74ae8172b239a4a4808fbe0
yuasys <2534721+yuasys@users.noreply.github.com>
2022-09-21 02:23:55 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>を編集しリロードのために保存しよう
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ja.reactjs.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          日本語公式サイトで学ぶ
         </a>
       </header>
     </div>
